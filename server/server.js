c47af9b71e9b021f5eb6322cb19fc1bc93357267
Thanh Long <thanhlongprrt@gmail.com>
2024-07-03 18:53:33 +0700

    #1 Code base

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 require('dotenv').config();
 
+const dbConnect = require('./config/dbconnect')
+
 const app = express();
 const port = process.env.PORT || 8888;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+dbConnect();
+
 app.use('/', (req, res) => { res.send("SERVER ON") });
 
 app.listen(port, () => {
