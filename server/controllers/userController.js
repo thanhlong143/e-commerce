99df47938549cc638dd6e522428cc862fe7889fc
Thanh Long <thanhlongprrt@gmail.com>
2024-07-04 03:17:50 +0700

    #3 Register & Login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/userModel');
+
 const asyncHandler = require('express-async-handler');
+
 const register = asyncHandler(async (req, res) => {
     const { email, password, firstname, lastname } = req.body;
     if (!email || !password || !firstname) {
@@ -8,13 +10,41 @@ const register = asyncHandler(async (req, res) => {
             message: 'Missing inputs'
         })
     }
-    const response = await User.create(req.body);
-    return res.status(200).json({
-        success: response ? true : false,
-        response
-    })
+
+    const user = await User.findOne({ email });
+    if (user) {
+        throw new Error('This user email address already exists');
+    } else {
+        const newUser = await User.create(req.body);
+        return res.status(200).json({
+            success: newUser ? true : false,
+            message: newUser ? 'Register is successfully, please login' : 'Something went wrong'
+        })
+    }
+});
+
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: 'Missing inputs'
+        })
+    }
+
+    const response = await User.findOne({ email });
+    if (response && await response.isCorrectPassword(password)) {
+        const { password, role, ...userData } = response.toObject();
+        return res.status(200).json({
+            success: true,
+            userData
+        })
+    } else {
+        throw new Error('Invalid credentials!');
+    }
 });
 
 module.exports = {
-    register
+    register,
+    login
 }
