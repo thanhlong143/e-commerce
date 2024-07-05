98abb9a24b72a1814a999ffeda823d41c7c7b7ba
Thanh Long <thanhlongprrt@gmail.com>
2024-07-05 14:16:46 +0700

    #9 CRUD Product

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
 const userRouter = require('./user');
+const productRouter = require('./product');
 const { notFound, errorHandler } = require('../middlewares/errorHandlerMiddleware');
 
 const initRoutes = (app) => {
     app.use('/api/user', userRouter);
-
-
+    app.use('/api/product', productRouter);
 
     app.use(notFound);
     app.use(errorHandler);
---
+++ server/routes/product.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const ctrls = require('../controllers/productController');
+const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken');
+
+router.post('/', [verifyAccessToken, isAdmin], ctrls.createProduct);
+router.get('/', ctrls.getProducts);
+router.get('/:pid', ctrls.getProduct);
+router.put('/:pid', [verifyAccessToken, isAdmin], ctrls.updateProduct);
+router.delete('/:pid', [verifyAccessToken, isAdmin], ctrls.deleteProduct);
+
+module.exports = router;
