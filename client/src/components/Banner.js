905b27e961fc6edbdb462acb99b961a1c88d194a
Thanh Long <thanhlongprrt@gmail.com>
2024-07-09 21:04:44 +0700

    #22 Header and Navigation

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Banner = () => {
     return (
-        <div>Banner</div>
+        <div className="w-full">
+            <img src="https://digital-world-2.myshopify.com/cdn/shop/files/slideshow3-home2_1920x.jpg?v=1613166679" className="w-full object-center" alt="banner" />
+        </div>
     )
 }
 
